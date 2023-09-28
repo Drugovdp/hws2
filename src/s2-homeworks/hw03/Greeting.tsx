a7424dc927dbc24aae3c2495015f343fc2a43845
Drugovdp <drugovdp@gmail.com>
2023-09-28 21:43:36 +0500

    Add module

--- src/s2-homeworks/hw03/Greeting.tsx
+++ src/s2-homeworks/hw03/Greeting.tsx
@@ -1,15 +1,15 @@
-import React, {ChangeEvent, KeyboardEvent} from 'react'
+import React, { ChangeEvent, KeyboardEvent } from 'react'
 import s from './Greeting.module.css'
 
 type GreetingPropsType = {
-    name: any // need to fix any
-    setNameCallback: any // need to fix any
-    addUser: any // need to fix any
-    onBlur: any // need to fix any
-    onEnter: any // need to fix any
-    error: any // need to fix any
-    totalUsers: any // need to fix any
-    lastUserName?: any // need to fix any
+    name: string // need to fix any
+    setNameCallback: (text: string) => void // need to fix any
+    addUser: () => void // need to fix any
+    onBlur: () => void // need to fix anys
+    onEnter: (enter: string) => void // need to fix any
+    error: string // need to fix any
+    totalUsers: number // need to fix any
+    lastUserName?: string // need to fix any
 }
 
 // презентационная компонента (для верстальщика)
@@ -25,47 +25,26 @@ const Greeting: React.FC<GreetingPropsType> = (
         lastUserName,
     } // деструктуризация пропсов
 ) => {
-    const inputClass = s.errorInput // need to fix with (?:)
-
+    const inputClass = error ? s.errorInput : s.input // need to fix with (?:)
+    
     return (
         <div id={'hw3-form'} className={s.greetingForm}>
-            <div className={s.text}>
-                {'Людей добавили: '}
-                <span id={'hw3-users-total'}>
-                    {totalUsers}
-                </span>
-            </div>
-
+            <div className={s.text}>{'Людей добавили: '}<span id={'hw3-users-total'}>{totalUsers}</span></div>
             <div className={s.inputAndButtonContainer}>
                 <div>
                     <input
                         id={'hw3-input'}
                         value={name}
-                        onChange={setNameCallback}
+                        onChange={(e: ChangeEvent<HTMLInputElement>) => setNameCallback(e.currentTarget.value)}
                         className={inputClass}
-                        onKeyDown={onEnter}
+                        onKeyDown={(e: KeyboardEvent<HTMLInputElement>)=>onEnter(e.key)}
                         onBlur={onBlur}
                     />
-                    <div id={'hw3-error'} className={s.error}>
-                        {error}
-                    </div>
+                    <div id={'hw3-error'} className={s.error}>{error}</div>
                 </div>
-
-                <button
-                    id={'hw3-button'}
-                    onClick={addUser}
-                    className={s.button}
-                    disabled={!name.trim()}
-                >
-                    add
-                </button>
+                <button id={'hw3-button'} onClick={addUser} className={s.button} disabled={!name.trim()}>add</button>
             </div>
-
-            {lastUserName && (
-                <div className={s.greeting}>
-                    Привет <span id={'hw3-last-user'}>{lastUserName}</span>!
-                </div>
-            )}
+            {!error ? <div className={s.greeting}>Привет <span id={'hw3-last-user'}>{lastUserName}</span>!</div> : ''}
         </div>
     )
 }
